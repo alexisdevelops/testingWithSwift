a5aaef49a39894964e62951495aedd9855670120
alexisdevelops <alexisdevelops@gmail.com>
2021-03-08 16:42:44 -0800

    Starting test coverage

---
+++ FirstTestingProject/FirstTestingProject/PrimeCalculator.swift
@@ -0,0 +1,28 @@
+//
+//  PrimeCalculator.swift
+//  FirstTestingProject
+//
+//  Created by alexis on 08/03/21.
+//
+
+import Foundation
+
+struct PrimeCalculator {
+    static func calculate(upTo max: Int) -> [Int] {
+        guard max > 1 else {
+            return []
+        }
+        var sieve = [Bool](repeating: true, count: max)
+        sieve[0] = false
+        sieve[1] = false
+        for number in 2 ..< max {
+            if sieve[number] == true {
+                for multiple in stride(from: number * number, to:
+                                        sieve.count, by: number) {
+                    sieve[multiple] = false
+                }
+            } }
+        let primes = sieve.enumerated().compactMap { $1 == true ? $0 : nil }
+        return primes
+    }
+}
